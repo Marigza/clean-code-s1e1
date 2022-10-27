76d7fc08cffbfeb7ea683c36873f5fd0b7fd8df8
Marigza <marigola@mail.ru>
2022-10-27 19:47:06 +0300

    feat: add classlist to li element in js-file, fix css-property, add whitespace in css-file

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ var completedTasksHolder=document.getElementById("completed-tasks");//completed-
 //New task list item
 var createNewTaskElement=function(taskString){
 
-    var listItem=document.createElement("li");
+    var listItem = document.createElement("li");
+    listItem.className = 'main-list__item'
 
     //input (checkbox)
     var checkBox=document.createElement("input");//checkbx
